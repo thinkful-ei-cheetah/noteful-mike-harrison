c23e0389b38259a3b44e3dd93e60f12d6190cf52
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-04-23 09:09:51 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dummyStore from './dummy-store';
+import Notelist from './Notelist/Notelist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  state ={
+    note: [],
+    folders:[]
+  }
+  componentDidMount(){
+    setTimeout(() => this.setState(dummyStore),1000)
+  }
+
+  render(){
+    const {notes}= this.state
+    return (
+      <>
+      <heading>
+        <h1>Noteful</h1>
+      </heading>
+      <Notelist notes={notes}/>
+      </>
+    )
+  }
 }
 
-export default App;
